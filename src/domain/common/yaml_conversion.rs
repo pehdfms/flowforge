196350ed7d7e52849783a21dc7f69eda2f99d5d5
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2023-03-08 17:03:19 -0300

    refactor: ominous message

--- src/domain/common/yaml_conversion.rs
+++ src/domain/common/yaml_conversion.rs
@@ -16,3 +16,13 @@ pub trait YamlConversion {
     fn to_yaml(&self) -> Yaml;
 }
 
+// TODO I'll probably have to completely scrap rust_yaml
+// it's a reasonably good library, but it doesn't support the exact
+// kind of yaml github actions needs
+// for example:
+//
+// "on":
+//   - push
+//
+// is the kind of code it would generate, but that's not a valid
+// github actions yaml file (the hyphen breaks it)
